2fd8fdd39d1d3b1aed280e61dee412f444c186a4
luojinghui2 <luojinghui@xylink.com>
2024-08-12 18:57:49 +0800

    chore: add more params to build lib[#000000]

--- core/index.mjs
+++ core/index.mjs
@@ -4,7 +4,7 @@
  * @authors Luo-jinghui (luojinghui424@gmail.com)
  *
  * Created at     : 2022-08-12 19:11:52
- * Last modified  : 2024-08-12 18:57:05
+ * Last modified  : 2024-08-12 18:57:43
  */
 
 import inquirer from 'inquirer';
@@ -177,9 +177,9 @@ class Publisher {
    */
   getPackageJsonPath() {
     const { packageJsonPath } = this.buildConfig;
-    const path = path.resolve(process.cwd(), packageJsonPath, 'package.json');
+    const packagePath = path.resolve(process.cwd(), packageJsonPath, 'package.json');
 
-    return path;
+    return packagePath;
   }
 
   parseTask() {
